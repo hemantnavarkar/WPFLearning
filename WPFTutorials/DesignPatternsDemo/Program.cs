fd2207e7e4b7bd0d8fd25c04835c52ae84aec592
hemant <hemantnavarkar@gmail.com>
2023-02-03 20:52:00 +0530

    Thread Demo

--- WPFTutorials/DesignPatternsDemo/Program.cs
+++ WPFTutorials/DesignPatternsDemo/Program.cs
@@ -1,2 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DesignPatternsDemo.ThreadDemo;
+using System.Diagnostics;
+using System.Threading;
+
+Console.WriteLine("***********************Thread Demo");
+ThreadDemo demo = new ThreadDemo();
+Stopwatch stopwatch = new Stopwatch();
+
+stopwatch.Start();
+// Sync Way
+////demo.PrintNumbers();
+
+
+////var threads = new Thread[100];
+////for (int threadCounter = 0; threadCounter < 100; threadCounter++)
+////{
+////    Thread printerThread = new(new ThreadStart(demo.PrintNumbers))
+////    {
+////        Name = $"Thread{threadCounter}",
+////        IsBackground = false
+////    };
+
+////    threads[threadCounter] = printerThread;
+////    printerThread.Start();
+////}
+
+
+
+////foreach (var thread in threads.ToList())
+////{
+////    thread.Join();
+////}
+
+for (int threadCounter = 0; threadCounter < 100; threadCounter++)
+{
+    demo.PrintNumbers();
+}
+
+
+// demo.resetEvent.WaitOne();
+
+
+
+stopwatch.Stop();
+Console.WriteLine($"Hemant = {stopwatch.Elapsed}");
+Console.ReadLine();
